9c3113b7c42ff656b86a0aa5b210783818ce55fe
Aloisz <a.tupinon@rubika-edu.com>
2024-02-14 16:13:34 +0100

    CAM scritp

---
+++ ProjetFPV-Unity/Assets/Script/Camera/CameraManager.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
+
+
+namespace CameraBehavior
+{
+    
+}
+
