20b5cda79b83454e63e17814ba3e5d579e6eba52
Aloiszz <a.tupinon@rubika-edu.com>
2024-02-20 22:44:56 +0100

    Tout debut des weapons

---
+++ ProjetFPV-Unity/Assets/Script/Weapon/WeaponManager.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+namespace Weapon
+{
+    public class WeaponManager : MonoBehaviour
+    {
+        //PlayerController PlayerController
+        private Vector3 shootingDir;
+        private bool canFire;
+        bool isShooting;
+        float lastTimefired;
+        private bool isReloading;
+        private int actualNumberOfBullet;
+
+        
+        private void Start()
+        {
+            
+        }
+        public void Update()
+        {
+            
+        }
+        
+    }
+}
+
+
