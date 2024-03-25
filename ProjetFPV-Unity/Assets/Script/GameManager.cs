0d129fc0acd82cfe954765ecf57c78e76a18e1da
Alois <a.tupinon@rubika-edu.com>
2024-03-25 17:03:59 +0100

    Trash mob behavior

--- ProjetFPV-Unity/Assets/Script/GameManager.cs
+++ ProjetFPV-Unity/Assets/Script/GameManager.cs
@@ -1,7 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using AI;
 using UnityEngine;
+using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 public class GameManager : GenericSingletonClass<GameManager>
 {
+    public int numberOfTrashMob = 20;
+    public List<AI_Pawn> aiPawnsAvailable;
+    private void Start()
+    {
+        for (int i = 0; i < numberOfTrashMob; i++)
+        {
+            GameObject trashMob = Pooling.instance.Pop("Trashmob");
+            trashMob.transform.position = Random.insideUnitSphere * 350;
+        }
+    }
 }
