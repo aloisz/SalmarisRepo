0107c91c5f25669a41f1168f52ab3c45ad63160c
aloisz <alois.tupinon@gmail.com>
2024-04-24 18:16:26 +0200

    MZr weight

--- ProjetFPV-Unity/Assets/Script/IA/AI_Pawn.cs
+++ ProjetFPV-Unity/Assets/Script/IA/AI_Pawn.cs
@@ -18,12 +18,13 @@ namespace AI
         [SerializeField] protected SO_IA so_IA;
         
         public LayerMask targetMask;
-        
         [Space]
         [ProgressBar("Health", 500, EColor.Red)] [SerializeField] protected float actualPawnHealth;
-        
         [Header("Tick")]
         [SerializeField][Tooltip("How many time the check is performed")] protected float tickVerification = 0.2f;
+
+        [Header("Director")]
+        public float enemyWeight;
         
         //Component----------------------
         internal NavMeshAgent navMeshAgent;
