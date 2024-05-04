d86c38319473e3db981f8933a88e9826f6574505
Aloiszz <a.tupinon@rubika-edu.com>
2024-05-04 16:55:44 +0200

    Audio manager working

--- ProjetFPV-Unity/Assets/Script/Sound/AudioManager.cs
+++ ProjetFPV-Unity/Assets/Script/Sound/AudioManager.cs
@@ -1,15 +1,49 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
-
+using UnityEngine.Audio;
+using UnityEngine.Serialization;
 
 
 namespace MyAudio
 {
     public class AudioManager : GenericSingletonClass<AudioManager>
     {
-        public List<SoundList> soundList;
-        public AudioSource audioToSpawn;
+        public List<AudioSO> audioSO;
+        public MyAudioSource PrefabAudioSource;
+
+        [SerializeField] private AudioMixer audioMixer;
+        private void Start()
+        {
+            SpawnAudio(transform.position, SfxType.Music, 0, .5f, 1, 0);
+        }
+
+        public void SpawnAudio(Vector3 position, SfxType sfxType, int audioID, float volume, float pitch, float spatialBlend, bool loop = false)
+        {
+            MyAudioSource audioSource = Instantiate(PrefabAudioSource, position, Quaternion.identity);
+
+            //audioSource.aSource.volume = volume;
+            audioSource.aSource.volume = 0;
+            audioSource.aSource.DOFade(volume, 1);
+            audioSource.aSource.pitch = pitch;
+            audioSource.aSource.spatialBlend = spatialBlend;
+            audioSource.aSource.loop = loop;
+            audioSource.aSource.outputAudioMixerGroup = audioMixer.FindMatchingGroups(sfxType.ToString())[0];
+             
+            foreach (var soundClip in audioSO[(int)sfxType].soundList)
+            {
+                if (audioID == soundClip.audioId)
+                {
+                    audioSource.aSource.clip = audioSO[(int)sfxType].soundList[audioID].audioClip;
+                    audioSource.timeBeforeDestroy = audioSO[(int)sfxType].soundList[audioID].audioDuration;
+                    audioSource.gameObject.name = audioSO[(int)sfxType].soundList[audioID].soundName;
+                }
+            }
+            
+            audioSource.aSource.Play();
+        }
     }
 
     public enum SfxType
@@ -20,18 +54,12 @@ namespace MyAudio
     }
 
     [System.Serializable]
-    public class SoundList
-    {
-        public SfxType sfxType;
-        public Sound sound;
-    }
-
-    [System.Serializable]
     public class Sound
     {
-        public string soundName;
-        public AudioSource audioClip;
         public int audioId;
+        public string soundName;
+        public float audioDuration;
+        public AudioClip audioClip;
     }
 }
 
