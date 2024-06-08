38dae165f27ffd917fd6ca4403f3c724deb9531f
Aloiszz <a.tupinon@rubika-edu.com>
2024-06-08 18:35:28 +0200

    OC scene Alois + debug frame rate

--- ProjetFPV-Unity/Assets/Script/GameManager.cs
+++ ProjetFPV-Unity/Assets/Script/GameManager.cs
@@ -8,7 +8,6 @@ using Random = UnityEngine.Random;
 
 public class GameManager : GenericSingletonClass<GameManager>
 {
-    public List<AI_Pawn> aiPawnsAvailable;
     public int currentLevelIndex;
     public int currentCheckpointIndex;
     
@@ -16,16 +15,29 @@ public class GameManager : GenericSingletonClass<GameManager>
 
     public int globalScore;
 
-    private void Start()
-    {
-        
-    }
-
     public void LevelFinished()
     {
         OnLevelCompleted?.Invoke();
         Debug.Log("Level Finished");
 
         currentLevelIndex++;
+    }
+    
+    
+    public int avgFrameRate;
+    public void Update ()
+    {
+        float current = 0;
+        current = Time.frameCount / Time.time;
+        avgFrameRate = (int)current;
+    }
+    
+    private void OnGUI()
+    {
+        GUIStyle font = new GUIStyle();
+        font.fontSize = 50;
+        font.fontStyle = FontStyle.Bold;
+        //font.font.material.color = Color.white;
+        GUI.Label(new Rect(5, 40, 100, 25), "FPS: " + Mathf.Round(avgFrameRate), font);
     }
 }
