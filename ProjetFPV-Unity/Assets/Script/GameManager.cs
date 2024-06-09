c33e0199580684db66332227ccc000522d3a3273
Aloiszz <a.tupinon@rubika-edu.com>
2024-06-09 11:45:00 +0200

    ms counter

--- ProjetFPV-Unity/Assets/Script/GameManager.cs
+++ ProjetFPV-Unity/Assets/Script/GameManager.cs
@@ -26,6 +26,7 @@ public class GameManager : GenericSingletonClass<GameManager>
     
     private  int avgFrameRate;
     private int frameRate;
+    private float ms;
     public void Update ()
     {
         float current = 0;
@@ -33,6 +34,8 @@ public class GameManager : GenericSingletonClass<GameManager>
         avgFrameRate = (int)current;
         
         frameRate = (int) (1f / Time.deltaTime);
+        
+        ms = Time.deltaTime * 1000;
     }
     
     private void OnGUI()
@@ -44,5 +47,7 @@ public class GameManager : GenericSingletonClass<GameManager>
         GUI.Label(new Rect(5, 40, 100, 25), "Average FPS: " + Mathf.Round(avgFrameRate), font);
         
         GUI.Label(new Rect(5, 90, 100, 25), "FPS: " + Mathf.Round(frameRate), font);
+        
+        GUI.Label(new Rect(5, 140, 100, 25), "ms: " + Mathf.Round(ms), font);
     }
 }
