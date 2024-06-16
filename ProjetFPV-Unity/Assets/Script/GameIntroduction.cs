869156ea70b9a5ca1b69c5742261201be1bc2162
Alois <a.tupinon@rubika-edu.com>
2024-06-16 17:22:33 +0200

    On avance sur l'intro

--- ProjetFPV-Unity/Assets/Script/GameIntroduction.cs
+++ ProjetFPV-Unity/Assets/Script/GameIntroduction.cs
@@ -15,9 +15,11 @@ public class GameIntroduction : MonoBehaviour
 
     [Header("WayPoint")] 
     [SerializeField] private Transform waitingDoorPos;
+    [SerializeField] private Transform doorDirection;
     [SerializeField] private Transform OutsidePos;
-    
-    
+
+    [Header("WayPoint")] 
+    [SerializeField] private AnimationCurve jumpingCurve;
     IEnumerator Start()
     {
         Init(false);
@@ -54,6 +56,7 @@ public class GameIntroduction : MonoBehaviour
         {
             case 5:
                 ShowWeapon();
+                
                 break;
             case 6:
                 FaceCameraToDoor();
@@ -77,13 +80,21 @@ public class GameIntroduction : MonoBehaviour
 
     private void JumpOutSide()
     {
-        CameraParent.transform.DORotate(OutsidePos.eulerAngles, 1.25f);
-        CameraParent.transform.DOMove(OutsidePos.position, .25f).OnComplete(() =>
+        CameraParent.transform.DORotate(doorDirection.eulerAngles, .5f);  
+        StartCoroutine(WaitToLookBeforeJump());
+        StartCoroutine(Jump());
+    }
+
+    private IEnumerator WaitToLookBeforeJump()
+    {
+        yield return new WaitForSeconds(2);
+        CameraParent.transform.DORotate(OutsidePos.eulerAngles, .2f);
+    }
+    private IEnumerator Jump()
+    {
+        yield return new WaitForSeconds(.8f);
+        CameraParent.transform.DOJump(OutsidePos.position, 10, 1, 1.25f).SetDelay(1).SetEase(jumpingCurve).OnComplete(() =>
             Init(true));
     }
     
-    void Update()
-    {
-        
-    }
 }
